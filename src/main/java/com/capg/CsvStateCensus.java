1550d8095de8df49a97fdbb27a6e302e0c62d325
Mehakjit <mehakjitsingh@gmail.com>
2020-10-23 05:58:20 +0530

    [Mehakjit] Added: Ability to Load Census Data from a CSV file

---
+++ src/main/java/com/capg/CsvStateCensus.java
@@ -0,0 +1,21 @@
+package com.capg;
+
+//import com.opencsv.bean.CsvBindByName;
+
+public class CsvStateCensus {
+
+	public String state;
+
+	public String population;
+
+	public String areaInSqKm;
+
+	public String densityPerSqKm;
+	@Override
+	public String toString() {
+		return "CsvStateCensus [" +"state='" + state + '\''+", population='" + population + '\''+ ", areaInSqKm='" + areaInSqKm+ '\''+
+			   ", densityPerSqKm='" + densityPerSqKm + '\''+ ']';
+	}
+	
+	
+}
