85bd727226cc8c659af79e269b169de33d9d01f1
Mehakjit <mehakjitsingh@gmail.com>
2020-10-27 06:21:54 +0530

    [Mehakjit] Refactor1B: To get count from CSV Iterator via genrics

--- src/main/java/com/capg/StateCensusAnalyser.java
+++ src/main/java/com/capg/StateCensusAnalyser.java
@@ -20,33 +20,31 @@ public class StateCensusAnalyser<E> {
 		}
 		try(Reader reader = Files.newBufferedReader(Paths.get(file))){
 			Iterator<IndiaStateCensus> csvStateCensusIterator = this.getCSVFileIterator(reader, IndiaStateCensus.class);
-			Iterable<IndiaStateCensus> csvIterable = () -> csvStateCensusIterator;
-			int no_of_Entries = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
-			return no_of_Entries;
+			return noOfEntries(csvStateCensusIterator);
 		} catch (IOException e) {
 			throw new StateCensusAnalyserException("File Doesn't Exist",StateCensusAnalyserException.ExceptionType.FILE_NOT_EXIST);
-		}catch (IllegalStateException e) {
-			throw new StateCensusAnalyserException("Unable to parse", StateCensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
 		}catch (RuntimeException e) {
 			throw new StateCensusAnalyserException("File Internal Error", StateCensusAnalyserException.ExceptionType.CSV_INTERNAL_ISSUE);
 		}
 	}
+	
 	public int loadStateCode(String csvFilePath) throws StateCensusAnalyserException{
 		if(! csvFilePath.contains(".csv")){
 			throw new StateCensusAnalyserException("Not .csv file", StateCensusAnalyserException.ExceptionType.WRONG_TYPE);
 		}
 		try(Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))){
 			Iterator<CsvStateCode> csvStateCodeIterator = this.getCSVFileIterator(reader, CsvStateCode.class);
-			Iterable<CsvStateCode> csvIterable = () -> csvStateCodeIterator;
-			int no_of_Entries = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
-			return no_of_Entries;
+			return noOfEntries(csvStateCodeIterator);
 		}  catch (IOException e) {
 			throw new StateCensusAnalyserException("File Doesn't Exist",StateCensusAnalyserException.ExceptionType.FILE_NOT_EXIST);
-		}catch (IllegalStateException e) {
-			throw new StateCensusAnalyserException("Unable to parse", StateCensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
 		}catch (RuntimeException e) {
 			throw new StateCensusAnalyserException("File Internal Error", StateCensusAnalyserException.ExceptionType.CSV_INTERNAL_ISSUE);
 		}
+	}
+	private <E> int noOfEntries(Iterator<E> CSVFileIterator) {
+		Iterable<E> csvIterable = () -> CSVFileIterator;
+		int no_of_Entries = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
+		return no_of_Entries;
 	}
 	
 	private <E> Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws StateCensusAnalyserException {
